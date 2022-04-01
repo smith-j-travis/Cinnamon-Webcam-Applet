a77bc5228b2a828bfb6f40a5fe7a310ec0d12135
Travis Smith <smith.j.travis@gmail.com>
2022-04-01 10:24:15 -0400

    update to work with cinnamon 5.2.7 and switched to async calls

--- applet.js
+++ applet.js
@@ -3,14 +3,59 @@ const PopupMenu = imports.ui.popupMenu;
 const St = imports.gi.St;
 const GLib = imports.gi.GLib;
 const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 
-class WidgetStatusApplet extends Applet.TextIconApplet {
-    updateIcon() {
-      let firstVideoDevice = backtick("ls -1 /dev").split("\n").find(function(device) {
+function WidgetStatusApplet(orientation, panel_height, instance_id) {
+    this._init(orientation, panel_height, instance_id);
+}
+
+WidgetStatusApplet.prototype = {
+    __proto__: Applet.IconApplet.prototype,
+
+    _init: function(orientation, panel_height, instance_id) {
+            Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
+
+            this.camProcess = "";
+            this.menuManager = new PopupMenu.PopupMenuManager(this);
+            this.menu = new Applet.AppletPopupMenu(this, orientation);
+            this.menuManager.addMenu(this.menu);
+            this._contentSection = new PopupMenu.PopupMenuSection();
+            this.menu.addMenuItem(this._contentSection);
+
+            let item = new PopupMenu.PopupIconMenuItem(_("Kill Webcam Process"), "window-close-symbolic", St.IconType.SYMBOLIC);
+
+            item.connect('activate', Lang.bind(this, function() {
+              try {
+                GLib.spawn_command_line_async("kill " + this.camProcess);
+              } catch(e) {
+                global.logError(e);
+              }
+            }));
+
+            this.menu.addMenuItem(item);
+
+            this._updateLoopID = Mainloop.timeout_add(2000, () => this.updateIcon());
+    },
+    backtick: function(command) {
+        try {
+          let [result, stdout, stderr] = GLib.spawn_command_line_async(command);
+          if (stdout != null) {
+            return stdout.toString();
+          }
+        }
+        catch (e) {
+          global.logError(e);
+        }
+
+        return "";
+   },
+
+    updateIcon: function() {
+      let firstVideoDevice = this.backtick("ls -1 /dev").split("\n").find(function(device) {
         return device.toLowerCase().indexOf("video") > -1;
       });
 
-      this.camProcess = backtick("fuser /dev/" + firstVideoDevice).trim();
+      this.camProcess = this.backtick("fuser /dev/" + firstVideoDevice).trim();
 
       if(this.camProcess == "") {
         this.set_applet_icon_name("webcam-off");
@@ -20,57 +65,21 @@ class WidgetStatusApplet extends Applet.TextIconApplet {
         this.set_applet_tooltip(_("Webcam is currently on"));
       }
 
-      if(this.running === true) {
-        setTimeout(this.updateIcon, 2000);
-      }
-    }
+      this._updateLoopID = Mainloop.timeout_add(2000, () => this.updateIcon());
+    },    
 
-    constructor(orientation, panel_height, instance_id) {
-        super(orientation, panel_height, instance_id);
-        
-        this.updateIcon = this.updateIcon.bind(this);
-        this.running = true;
-        this.camProcess = "";
-
-        this.menuManager = new PopupMenu.PopupMenuManager(this);
-        this.menu = new Applet.AppletPopupMenu(this, orientation);
-        this.menuManager.addMenu(this.menu);
-        this._contentSection = new PopupMenu.PopupMenuSection();
-        this.menu.addMenuItem(this._contentSection);
-
-        let item = new PopupMenu.PopupIconMenuItem(_("Kill Webcam Process"), "window-close-symbolic", St.IconType.SYMBOLIC);
-        item.connect('activate', Lang.bind(this, function() {
-          GLib.spawn_command_line_sync("kill " + this.camProcess);
-        }));
-        this.menu.addMenuItem(item);
-
-        this.updateIcon();
-    }
-
-    on_applet_clicked(event) {
+    on_applet_clicked: function() {
       if(this.camProcess != "") {
         this.menu.toggle();
       }
-    }
+    },
 
-    on_applet_removed_from_panel() {
-      this.running = false;
+    on_applet_removed_from_panel: function() {
+        if(this._updateLoopID) {
+            Mainloop.source_remove(this._updateLoopID);
+        }
     }
-}
-
-function backtick(command) {
-    try {
-      let [result, stdout, stderr] = GLib.spawn_command_line_sync(command);
-      if (stdout != null) {
-        return stdout.toString();
-      }
-    }
-    catch (e) {
-      global.logError(e);
-    }
-  
-    return "";
-  }
+};
 
 function main(metadata, orientation, panel_height, instance_id) {
     return new WidgetStatusApplet(orientation, panel_height, instance_id);
